5b9c1f080cfdcb0e3f0804eb5e8a3762d499a588
Joey <85124401+joeyc11@users.noreply.github.com>
2023-01-21 16:54:51 -0500

    Update webscrape.py

--- webscrape.py
+++ webscrape.py
@@ -5,7 +5,9 @@ page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
 soup.body.get_text('\n', strip=True)
 soup.body.get_text('', strip=True)
-string = [i.text for i in soup.findAll({"div":{"class":"texts"}})]
-for s in range(len(string)):
-    string[s] = string[s].replace("\n","")
-print(string)
+siteText = [i.text for i in soup.findAll({"div":{"class":"texts"}})]
+for s in range(len(siteText)):
+    siteText[s] = siteText[s].replace("\n","")
+with open('readme.txt', 'w') as f:
+    for s in siteText:
+        f.write(s)
